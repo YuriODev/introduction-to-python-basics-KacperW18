26224383dda85ad673590fe8c65ad38ede59e44b
KacperW18 <161600887+KacperW18@users.noreply.github.com>
2024-02-28 19:45:47 +0000

    Exercise 4 and 5

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,10 +1,12 @@
 # Exercise 4
 # Your solution comes here
+
 input = int(input("Enter a four digit number"))
 num1 = input // 1000
 num2 = (input % 1000) // 100
 num3 = (input % 100) // 10
 num4 = input % 10
+
 
 result = (num1 + num2) // (num3 + num4)
 print(result)
--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -1,2 +1,8 @@
 # Exercise 5
 # Your solution comes here
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+if a > b:
+    print(a)
+else:
+    print(b)
