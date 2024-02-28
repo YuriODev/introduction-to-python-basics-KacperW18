61ca941f5b401bc51296f3386312a5f0cf73f32a
Kacper Wojna <5515635-KacperWojna@users.noreply.replit.com>
2024-02-28 13:20:03 +0000

    Exercise 1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,6 @@
 # Exercise 1
 # Your solution comes here
+number = int(input("Enter a five digit number: "))
+first = (number // 10000) + (number //100 % 10) + (number % 10)
+second = (number // 1000 % 10) + (number // 10 % 10)
+print(str(first)+str(second))
