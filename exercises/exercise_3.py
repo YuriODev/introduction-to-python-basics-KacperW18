d28440be9fbf8233e5a7e9947b8c8df0e8b9f537
KacperW18 <161600887+KacperW18@users.noreply.github.com>
2024-02-28 15:04:57 +0000

    Exercise 3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,7 @@
 # Exercise 3
 # Your solution comes here
+n = int(input("Enter how many seconds since the begining of the day: "))
+hours = n // 3600 % 24
+minutes = (n % 3600) // 60
+seconds = (n % 3600) % 60
+print(f"{hours}:{minutes}:{seconds}")
