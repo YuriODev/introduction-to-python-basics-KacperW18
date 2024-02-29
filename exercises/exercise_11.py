0bf4fc17d3b0297d9cbaa6390b7822c0117fa970
KacperW18 <161600887+KacperW18@users.noreply.github.com>
2024-02-29 10:19:48 +0000

    Exercise 11

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -3,7 +3,7 @@
 s = int(input())
 fhundred = s // 500
 hundred = (s % 500) // 100
-ten = (s % 50) // 10
+ten = (s % 100) // 10
 five = (s % 10) // 5
 one = (s % 5) // 1
 print(fhundred,"(500),",hundred,"(100),",ten,"(10),",five,"(5),",one,"(1)")
